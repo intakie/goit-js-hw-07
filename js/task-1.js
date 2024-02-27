f20a197bb11aa80eab6cece1c646bc2dd2f34e7b
intakie <intakiea@gmail.com>
2024-02-28 00:46:40 +0200

    add task-1 js css

--- js/task-1.js
+++ js/task-1.js
@@ -10,3 +10,37 @@ items.forEach(item => {
   console.log(`Categoty: ${categoryName}`);
   console.log(`Element: ${itemNum}`);
 });
+
+const head = document.querySelector('head');
+const link = '<link rel="stylesheet" href="./css/task-1.css" />';
+head.insertAdjacentHTML('beforeend', link);
+const font =
+  '<link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">';
+head.insertAdjacentHTML('beforeend', font);
+
+categories.classList.add('categories-list');
+
+const headers = document.querySelectorAll('h2');
+
+headers.forEach(header => {
+  header.classList.add('title');
+});
+
+const lists = document.querySelectorAll('ul');
+
+lists.forEach(list => {
+  list.classList.add('list');
+});
+
+categories.classList.remove('list');
+
+const listItems = document.querySelectorAll('li');
+
+listItems.forEach(listItem => {
+  listItem.classList.add('list-item');
+});
+
+const genLists = document.querySelectorAll('.item');
+genLists.forEach(genList => {
+  genList.classList.remove('list-item');
+});
